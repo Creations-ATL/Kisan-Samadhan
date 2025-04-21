c6b0354b434ba0a3e3908856ef5f255d8f4faa53
Creations-ATL <atl.projects2024@gmail.com>
2025-04-21 12:35:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-
-    window.botpress.on("webchat:ready", () => window.botpress.open());
-    window.botpress.init({
-      botId: "28e8d0bd-18cc-4875-b48e-9a588602f6d1",
-      clientId: "fab09bef-1339-44c8-ada9-ed8ee9402bad",
-      selector: "#webchat",
-      configuration: {
-        composerPlaceholder: "Type your farming question…",
-        botName: "Kisan Samadhan",
-        botAvatar: "https://files.bpcontent.cloud/2025/04/21/05/20250421051255-GAEI62NH.png",
-        color: "#e9933d",
-        variant: "solid",
-        themeMode: "light",
-        fontFamily: "rubik",
-        radius: 4,
-        allowFileUpload: true,
-        additionalStylesheetUrl: "https://files.bpcontent.cloud/2025/04/21/04/20250421043425-YMVB7EVX.css"
-      }
-    });
-
+window.botpressWebChat.init({
+  botId: "28e8d0bd-18cc-4875-b48e-9a588602f6d1",
+  clientId: "fab09bef-1339-44c8-ada9-ed8ee9402bad",
+  selector: "#webchat",
+  composerPlaceholder: "Type your farming question…",
+  botName: "Kisan Samadhan",
+  botAvatarUrl: "https://files.bpcontent.cloud/2025/04/21/05/20250421051255-GAEI62NH.png",
+  stylesheet: "https://files.bpcontent.cloud/2025/04/21/04/20250421043425-YMVB7EVX.css",
+  useSessionStorage: true
+});
